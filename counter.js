dd1a52bb410c42c5ec68cfbc4cea461bb326ba5f
Nagireddi Mahita <140743991+nagieddimahita@users.noreply.github.com>
2023-07-28 15:13:28 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,34 @@
+let updatevalue1 = document.getElementById("updatevalue")
+function onincrement() {
+    let a = updatevalue1.textContent;
+    let countervalue = parseInt(a) + 1;
+    if (countervalue>0){
+        updatevalue1.style.color = "green";
+    }
+    else if(countervalue<0){
+        updatevalue1.style.color = "red";
+    }
+    else {
+        updatevalue1.style.color="black";
+    }
+    updatevalue1.textContent = countervalue;
+}
+function onreset1() {
+    let countervalue = 0;
+    updatevalue1.textContent = countervalue;
+    updatevalue1.style.color = "black";
+}
+function ondecrement() {
+    let a = updatevalue1.textContent;
+    let countervalue = parseInt(a) - 1;
+    if (countervalue>0){
+        updatevalue1.style.color = "green";
+    }
+    else if(countervalue<0){
+        updatevalue1.style.color = "red";
+    }
+    else {
+        updatevalue1.style.color="black";
+    }
+    updatevalue1.textContent = countervalue;
+}
